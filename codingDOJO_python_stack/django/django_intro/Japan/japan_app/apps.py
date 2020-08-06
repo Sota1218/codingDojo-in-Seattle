bc2a350facfd4bd9f526b739336c64486567a1d9
Sota Yoshihiro <sotayoshihiro@USERnoMacBook-Air.local>
2020-08-06 18:36:03 +0900

    Remove duplicated directory

--- codingDOJO_python_stack/django/django_intro/Japan/japan_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JapanAppConfig(AppConfig):
-    name = 'japan_app'
--- codingDOJO_python_stack/django/django_intro/Japan/japan_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('aaa', views.aaa),
-    path('sss', views.sss),
-]
